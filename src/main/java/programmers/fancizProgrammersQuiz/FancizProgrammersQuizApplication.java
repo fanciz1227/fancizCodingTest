997b801e7218ba7e299440c13e9b63596567227c
fanciz1227 <fanciz1227@gmail.com>
2021-08-19 20:09:19 +0900

    Initial commit

---
+++ src/main/java/programmers/fancizProgrammersQuiz/FancizProgrammersQuizApplication.java
@@ -0,0 +1,13 @@
+package programmers.fancizProgrammersQuiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FancizProgrammersQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FancizProgrammersQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/programmers/fancizProgrammersQuiz/FancizProgrammersQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package programmers.fancizProgrammersQuiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FancizProgrammersQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
