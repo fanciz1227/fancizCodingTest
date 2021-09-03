ade131eeefee110df0678cd15b3c1c5db5e95c3b
유제근 <fanciz@wemakeprice.com>
2021-09-03 18:58:00 +0900

    fanciz Coding Test -Daily Coding Test Lv1 : 21-09-03

--- src/test/java/codingtest/fancizCodingTest/programmers/LevelOneTest.java
+++ src/test/java/codingtest/fancizCodingTest/programmers/LevelOneTest.java
@@ -1064,4 +1064,36 @@ public class LevelOneTest {
         System.out.println(stopWatch.prettyPrint());
         assertThat(answer, is(result));
     }
+
+    /**
+     * 새로 생긴 놀이기구는 인기가 매우 많아 줄이 끊이질 않습니다. 이 놀이기구의 원래 이용료는 price원 인데, 놀이기구를 N 번 째 이용한다면 원래 이용료의 N배를 받기로 하였습니다.
+     * 즉, 처음 이용료가 100이었다면 2번째에는 200, 3번째에는 300으로 요금이 인상됩니다.
+     * 놀이기구를 count번 타게 되면 현재 자신이 가지고 있는 금액에서 얼마가 모자라는지를 return 하도록 solution 함수를 완성하세요.
+     * 단, 금액이 부족하지 않으면 0을 return 하세요.
+     */
+    @Test
+    public void 부족한_금액_계산하기() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        final long answer = 10;
+        int price = 3;
+        int money = 20;
+        int count = 4;
+        long rPrice = 0;
+
+        for(int i=1; i<=count; i++) {
+            rPrice += (price * i);
+        }
+
+        long result = money > rPrice ? 0 : (money - rPrice) * -1;
+
+        //등차수열의 합을 이용해 이용료를 계산하고 금액이 부족하지 않은지 판단을 위해 math max를 사용해서 한줄로 풀은 답안이다.
+        //수학을 잘해야하나 싶다.. ㅠ
+        //return Math.max(price * (count * (count + 1) / 2) - money, 0);
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        assertThat(answer, is(result));
+    }
 }
