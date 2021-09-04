2ee3bc27b8882f12d251e508812433ab8d991a1a
유제근 <fanciz@wemakeprice.com>
2021-09-04 22:57:44 +0900

    fanciz Coding Test -Daily Coding Test Lv1 : 21-09-04

--- src/test/java/codingtest/fancizCodingTest/programmers/LevelOneTest.java
+++ src/test/java/codingtest/fancizCodingTest/programmers/LevelOneTest.java
@@ -1215,4 +1215,28 @@ public class LevelOneTest {
         System.out.println(stopWatch.prettyPrint());
         assertThat(answer, is(sb.toString()));
     }
+
+    /**
+     * 길이가 같은 두 1차원 정수 배열 a, b가 매개변수로 주어집니다. a와 b의 내적을 return 하도록 solution 함수를 완성해주세요.
+     * 이때, a와 b의 내적은 a[0]*b[0] + a[1]*b[1] + ... + a[n-1]*b[n-1] 입니다. (n은 a, b의 길이)
+     * a와 b의 내적은 1*(-3) + 2*(-1) + 3*0 + 4*2 = 3 입니다.
+     */
+    @Test
+    public void 내적() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        final int answer = -2;
+        int[] a = {-1,0,1};
+        int[] b = {1,0,-1};
+        int result = 0;
+
+        for (int i=0; i<a.length; i++) {
+            result += a[i] * b[i];
+        }
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        assertThat(answer, is(result));
+    }
 }
