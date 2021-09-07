fa432531358a4c695f19e14ce6f0644d0924048c
유제근 <fanciz@wemakeprice.com>
2021-09-07 21:03:53 +0900

    fanciz Coding Test -Daily Coding Test Lv1 : 21-09-07, quiz=보물지도

--- src/test/java/codingtest/fancizCodingTest/programmers/LevelOneTest.java
+++ src/test/java/codingtest/fancizCodingTest/programmers/LevelOneTest.java
@@ -1369,4 +1369,66 @@ public class LevelOneTest {
         System.out.println(stopWatch.prettyPrint());
         assertThat(answer, is(result));
     }
+
+    /**
+     * 네오는 평소 프로도가 비상금을 숨겨놓는 장소를 알려줄 비밀지도를 손에 넣었다. 그런데 이 비밀지도는 숫자로 암호화되어 있어 위치를 확인하기 위해서는 암호를 해독해야 한다. 다행히 지도 암호를 해독할 방법을 적어놓은 메모도 함께 발견했다.
+     * 지도는 한 변의 길이가 n인 정사각형 배열 형태로, 각 칸은 "공백"(" ") 또는 "벽"("#") 두 종류로 이루어져 있다.
+     * 전체 지도는 두 장의 지도를 겹쳐서 얻을 수 있다. 각각 "지도 1"과 "지도 2"라고 하자. 지도 1 또는 지도 2 중 어느 하나라도 벽인 부분은 전체 지도에서도 벽이다. 지도 1과 지도 2에서 모두 공백인 부분은 전체 지도에서도 공백이다.
+     * "지도 1"과 "지도 2"는 각각 정수 배열로 암호화되어 있다.
+     * 암호화된 배열은 지도의 각 가로줄에서 벽 부분을 1, 공백 부분을 0으로 부호화했을 때 얻어지는 이진수에 해당하는 값의 배열이다.
+     */
+    @Test
+    public void 비밀지도() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        final String[] answer = {"######", "###  #", "##  ##", " #### ", " #####", "### # "};
+        int[] arr1 = {46, 33, 33 ,22, 31, 50};
+        int[] arr2 = {27 ,56, 19, 14, 14, 10};
+        int n = 6;
+        String[] result = new String[n];
+
+        //정사각형 변의 길이 n만큼 반복한다.
+        for(int i=0; i<n; i++) {
+            //arr1,arr2 배열의 요소를 변의 길이만큼 2진수로 변환
+            char[] binaryArr1 = decimalToBinary(n, arr1[i]);
+            char[] binaryArr2 = decimalToBinary(n, arr2[i]);
+
+            StringBuilder sb = new StringBuilder();
+
+            //변의 길이만큼 10진수에서 2진수로 변환했기 때문에 동일하게 n만큼 반복한다.
+            for(int j=0; j<n; j++) {
+                //지도 1 또는 지도 2 중 어느 하나라도 벽인 부분은 전체 지도에서도 벽이다.
+                if(binaryArr1[j] == '1' || binaryArr2[j] == '1') {
+                    sb.append("#");
+                } else {
+                    sb.append(" ");
+                }
+            }
+
+            result[i] = sb.toString();
+        }
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        assertThat(answer, is(result));
+    }
+
+    //10진수 -> 2진수 변환
+    private char[] decimalToBinary(int n, int decimal) {
+        StringBuilder binaryStr = new StringBuilder();
+
+        //지도의 변이 5나 6이냐에 따라서 2진수 표현도 동일하게 변의 길이에 맞춰줘야하기 때문에 while이 아닌 for문으로 변의 길이 만큼 돌려준다.
+        //ex) n=5일때 10을 변환하면 01010이다. n=6일때 10을 변환하면 001010이다.
+        //따라서 n의 길이 만큼 2진수의 길이도 맞춰줘야한다.
+        //끙.. Integer.toBinaryString(33 | 56) | 연산자를 이용해서 2개의 값을 넘겨주면 우리가 구해야하는 값이 그대로 출력된다..
+        //다만 Integer.toBinaryString을 이용한 2진수 변환도 결국 변의 길이만큼 자리수를 맞춰줘야해서 소인수분해로 진행했다.
+        for(int i=0; i<n; i++) {
+            binaryStr.append(decimal % 2);
+            decimal /= 2;
+        }
+
+        //소인수분해로 했기 때문에 결과가 뒤집힌 상태로 나오기 때문에 다시 한번 뒤집어서 정상적으로 표현해준다.
+        return binaryStr.reverse().toString().toCharArray();
+    }
 }
