b7508258249ba11fdaeb782573e16a2ce6941df4
fanciz1227 <fanciz1227@gmail.com>
2021-10-04 22:12:17 +0900

    fanciz Coding Test -Daily Coding Test Lv2 : 21-10-04, quiz=기능개발

--- src/test/java/codingtest/fancizCodingTest/programmers/LevelTwoTest.java
+++ src/test/java/codingtest/fancizCodingTest/programmers/LevelTwoTest.java
@@ -823,4 +823,59 @@ public class LevelTwoTest {
             this.location = locate;
         }
     }
+
+    /**
+     * 프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
+     * 또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
+     * 먼저 배포되어야 하는 순서대로 작업의 진도가 적힌 정수 배열 progresses와 각 작업의 개발 속도가 적힌 정수 배열 speeds가 주어질 때 각 배포마다 몇 개의 기능이 배포되는지를 return 하도록 solution 함수를 완성하세요.
+     */
+    @Test
+    public void 기능개발() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        int[] answer = {2, 1};
+        int[] progresses = {93, 30, 55};
+        int[] speeds = {1, 30, 5};
+        int[] result = {0};
+
+        Queue<Comp> queue = new LinkedList<>();
+        List<Integer> completeList = new ArrayList<>();
+
+        for (int i=0; i<progresses.length; i++) {
+            int percent = (int) Math.ceil((100 - progresses[i]) / (double) speeds[i]);
+            System.out.println(percent);
+            queue.offer(new Comp(progresses[i], speeds[i], percent));
+        }
+
+        int cnt = 0;
+        int day = 0;
+        boolean end = false;
+
+        while (!queue.isEmpty()) {
+            Comp cmp = queue.peek();
+
+            if (day < cmp.completeDate) {
+                queue.poll();
+                cnt++;
+                end = true;
+            }
+        }
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        assertThat(answer, is(result));
+    }
+
+    private class Comp {
+        int progress;
+        int speed;
+        int completeDate;
+
+        Comp(int progress, int speed, int completeDate) {
+            this.progress = progress;
+            this.speed = speed;
+            this.completeDate = completeDate;
+        }
+    }
 }
