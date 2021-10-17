a0533d1d52f75818e5ab6cd6b6b32909ff1b58fd
fanciz1227 <fanciz1227@gmail.com>
2021-10-17 20:23:17 +0900

    fanciz Coding Test -Daily Coding Test Lv2 : 21-10-17, quiz=two개_이하로_다른_비트

--- src/test/java/codingtest/fancizCodingTest/programmers/LevelTwoTest.java
+++ src/test/java/codingtest/fancizCodingTest/programmers/LevelTwoTest.java
@@ -1164,4 +1164,34 @@ public class LevelTwoTest {
         System.out.println(stopWatch.prettyPrint());
         assertThat(answer, is(result));
     }
+
+    /**
+     * 양의 정수 x에 대한 함수 f(x)를 다음과 같이 정의합니다.
+     * x보다 크고 x와 비트가 1~2개 다른 수들 중에서 제일 작은 수
+     * 예를 들어,
+     * f(2) = 3 입니다. 다음 표와 같이 2보다 큰 수들 중에서 비트가 다른 지점이 2개 이하이면서 제일 작은 수가 3이기 때문입니다.
+     * f(7) = 11 입니다. 다음 표와 같이 7보다 큰 수들 중에서 비트가 다른 지점이 2개 이하이면서 제일 작은 수가 11이기 때문입니다.
+     * 정수들이 담긴 배열 numbers가 매개변수로 주어집니다. numbers의 모든 수들에 대하여 각 수의 f 값을 배열에 차례대로 담아 return 하도록 solution 함수를 완성해주세요.
+     */
+    @Test
+    public void two개_이하로_다른_비트() {
+        StopWatch stopWatch = new StopWatch();
+        stopWatch.start();
+
+        final int[] answer = {3, 11};
+        int[] numbers = {2, 7};
+        int[] result = {};
+
+        for (int i=0; i<numbers.length; i++) {
+            int fx = numbers[i];
+
+            while (true) {
+
+            }
+        }
+
+        stopWatch.stop();
+        System.out.println(stopWatch.prettyPrint());
+        assertThat(answer, is(result));
+    }
 }
